585cb7841d9e449066f5fd0bba386de8d8ec5dd9
Chethan S H <chethan.sh@gmail.com>
2024-01-05 08:29:26 +0530

    add HOME-MANAGER

--- user/home.nix
+++ user/home.nix
@@ -1,71 +1,79 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "csh";
   home.homeDirectory = "/home/csh";
+  home.stateVersion = "23.11"; 
+  programs.home-manager.enable = true;
+  nixpkgs.config.allowUnfree = true;
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "23.11"; # Please read the comment before changing.
+  home.packages = with pkgs; [
+  
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
+    ############################################################################
+    
+    # Version-Control
+    git
 
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+    # Text-Editors
+    neovim vscode-fhs
 
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+    # Shell
+    fish
+    
+    # Terminal-Emulators
+    terminator kitty
+
+    # Terminal-Tools
+    htop pfetch
+
+    # Terminal-Multiplexing & Session-Management
+    abduco dvtm
+
+    # Rust-Utilities
+    bat eza procs fd ripgrep
+    # Astro-Nvim
+    bottom lazygit
+
+    # Utils
+    tree unzip 
+
+    ############################################################################
+    
+    # Browsers
+    qutebrowser
+
+    # MultiMedia
+    flameshot mpv
+
+    # Office & Notes
+    libreoffice cherrytree #obsidian
+
+    # Networking
+    curl openvpn
+
+    # Password-Manager
+    keepass keepassxc
+
+    ############################################################################
+    
+    # Programming-Languages
+    nodejs_21 typescript python3
+
+    # NodeJs Global Packages
+    nodePackages.nodemon
+    nodePackages.ts-node
+    nest-cli
+
+    # Compilers
+    ghc gcc
+
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+  programs.git = {
+    enable = true;
+    userName = "Chethan S H";
+    userEmail = "chethan.sh@gmail.com";
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. If you don't want to manage your shell through Home
-  # Manager then you have to manually source 'hm-session-vars.sh' located at
-  # either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/csh/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    # EDITOR = "emacs";
-  };
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
 }
